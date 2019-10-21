d908aaa59468f0ddeb0c4d742bd02bb65bf556ab
lrakai <lmrakai@gmail.com>
2019-10-20 18:56:43 -0600

    Slim down removal script

--- remove_lab.sh
+++ remove_lab.sh
@@ -2,11 +2,9 @@
 
 eval $(sed 's/^\$//' ./Variables.ps1)
 
-resourceGroupScope=$(az group show --name $Lab -o tsv --query id)
-
-az policy assignment delete --name $PolicyAssignmentName --scope $resourceGroupScope
-az policy definition delete --name $PolicyDefinitionName 
-az role assignment delete --assignee $User -g $Lab -role $RoleDefinitionName
-az role definition delete --name $RoleDefinitionName --custom-role-only true
+az policy assignment delete --name "$PolicyAssignmentName" -g $Lab
+az policy definition delete --name "$PolicyDefinitionName" 
+az role assignment delete --assignee $User -g $Lab --role "$RoleDefinitionName"
+az role definition delete --name "$RoleDefinitionName" --custom-role-only true
 az ad user delete --id $User
 az group delete --name $Lab --yes
